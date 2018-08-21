3492352e4f8b825c8767df76d3577bcf4037cdf8
USER-20171010BN\Administrator <shishy_job@163.com>
2018-08-21 22:23:24 +0800

    1

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map map = new HashMap<String,String>();
+        map.put("a","a");
+    }
+}
